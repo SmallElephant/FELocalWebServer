e4d46a08e1136b9ec0f342a8e7112f0547081fc2
FlyElephant <keso_xiang@talkmate.com>
2016-06-06 17:33:48 +0800

    Initial Commit

---
+++ FELocalWebServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FELocalWebServer
+//
+//  Created by FlyElephant on 16/6/6.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
